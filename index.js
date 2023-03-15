d79e514b5c8879eb5bb8de7606e647bca3d37c43
philipmo <philipmo59@gmail.com>
2023-03-14 23:45:16 -0400

    not finished

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution here
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+let arrOfName = []; 
+
+for(const val of drivers) {
+	arrOfName.push(val.name); 
+}
+function findMatching(arrayOfObjects, string) {
+  let res = arrayOfObjects.filter(val => {
+    return (val && (val === string || val.toLowerCase().includes(string.toLowerCase())))
+  });
+
+  return res;
+
+
+}
+
+findMatching(arrOfName, "Bobby")
+
